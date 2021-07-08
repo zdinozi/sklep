70c7279c596dd0f386caf3dc355205f0e041592a
Wiktor Banasiak <zdi724@gmail.com>
2021-07-08 23:37:30 +0200

    Działające dodawanie rzeczy do skepu

--- add_item.php
+++ add_item.php
@@ -9,17 +9,40 @@
     <form action='' method='post'>
         <h3>WYSTAW PRZEDMIOT NA SPRZEDAŻ</h3>
     Nazwa przedmiotu: <input type='text' name='przedmiot'><br/>
-    Cena: <input type='number' name='cena'><br/>
+    Cena: <input type='number' step='0.01' name='cena'><br/>
     <input type='submit' value='DODAJ'>
     </form>
     <?php 
-    $przedmiot=$_POST['przedmiot'];
-    $cena=$_POST['cena'];
-    $conn=mysqli_connect('localhost','root','','uzytkownicy') or die ('Błędne połączenie z bazą danych.');
-    $id=
-    
-    
+        if(isset($_POST['przedmiot']))
+        {
+            $przedmiot=$_POST['przedmiot'];
+            $cena=$_POST['cena'];
+            $name=$_COOKIE['logowanie'];
+            $conn=mysqli_connect('localhost','root','','uzytkownicy') or die ('Błędne połączenie z bazą danych.');
+            $sql='SELECT id FROM uzytkownicy where login="'.$name.'"';
+            $wynik=mysqli_query($conn,$sql) or die ('Błedne zapytanie.');
+            $ile=mysqli_num_rows($wynik);
+            for($i=0;$i<$ile;$i++)
+            {
+                $wiersz=mysqli_fetch_row($wynik);
+            }
+            $sql1="INSERT INTO `przedmioty` VALUES (NULL,'".$przedmiot."','".$wiersz[0]."','".$cena."',NULL)";
+            $wynik2=mysqli_query($conn,$sql1) or die ('Błedne zapytanie.');
+            mysqli_close($conn);
+        }
     
     ?>
+    <div id='logout' style='position: fixed; right:0; top:0;'>
+    <form action='logout.php' method='post'>
+    <input type='submit' value='LOGOUT'>
+    <input type='button' value='PRZEGLĄDAJ SKLEP' onclick='shop()'>
+    <script>
+        function shop()
+        {
+            window.open('markt.php','_self');
+        }
+    </script>
+    </form>
+        </div>
 </body>
 </html>
